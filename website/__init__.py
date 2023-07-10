b6c9392d0175f9e0a4cde0c8177a7254350c499c
dhannywi <dwindrakusuma@gmail.com>
2023-07-09 22:23:03 -0500

    first iteration of app + jinja template

--- website/__init__.py
+++ website/__init__.py
@@ -5,4 +5,11 @@ def create_app():
   app = Flask(__name__)
   # encrypt cookie or session data
   app.config['SECRET_KEY'] = 'secure_NOTES_app'
+
+  from .views import views
+  from .auth import auth
+  # register blueprint
+  app.register_blueprint(views, url_prefix='/')
+  app.register_blueprint(auth, url_prefix='/')
+
   return app
